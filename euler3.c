c8d1c37e286b5ef2222222cc94554817e28cd08d
saiprakash.nalubolu <saiprakash.nalubolu@gmail.com>
2016-12-22 08:35:26 +0300

    modified code

--- euler3.c
+++ euler3.c
@@ -1,24 +1,34 @@
 #include<stdio.h>
+#include<stdbool.h>
 
-int main() {
-long long int n=600851475143;
-long int largest_prime_factor=0;
-long int i=0,factor=0,j=0;
-
-for (i=2;i<n;i++) {
-       n%i==0; 
-       factor=i;
-    for (j=2;j<i;j++) {
-      if (factor%j==0) {
-        break;
-       }
-      else{
-        largest_prime_factor=i;
-      }
+bool isPrime (long long n) {
+  for (long long ii=2; ii<n; ii++) {
+    if ((n%ii) == 0) {
+      return false;
     }
+  }
+  return true;
 }
 
-printf("largest prime factor for 600851475143 is %d",largest_prime_factor);
+int main () {
+  long long num=600851475143;
+  long long prime_factor=0,factor;
+  long long max_pri_fac=0;
 
-return 0;
+  for (factor=2;factor<num;factor++) {
+    if ((num % factor) == 0){
+      if (isPrime(factor)) {
+       prime_factor=factor;
+      
+       if (prime_factor>max_pri_fac) {
+         max_pri_fac=prime_factor;
+       }
+     }
+    }
+  }
+    
+  printf("largest prime factor is %lld",max_pri_fac);
+
+
+  return 0;
 }
